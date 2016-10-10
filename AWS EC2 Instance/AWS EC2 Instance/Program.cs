574dfa21b1c909673805589f8892cc621d47198a
Gowtham Reddy (09003111) <REDDYGK1@student.op.ac.nz>
2016-10-10 23:01:57 +1300

    New Program, as I just deleted my finished one

---
+++ AWS EC2 Instance/AWS EC2 Instance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWS_EC2_Instance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
